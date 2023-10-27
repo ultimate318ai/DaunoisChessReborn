d68117e405001f0b9033f20fad5933e687f4b869
Nathan DAUNOIS <nathandaunois@sfr.fr>
2023-10-27 21:45:26 +0200

    piece type and url picture function
    Signed-off-by: Nathan DAUNOIS <nathandaunois@sfr.fr>

--- DaunoisChessReborn/src/app/app.module.ts
+++ DaunoisChessReborn/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChessBoardComponent } from './chess-board/chess-board.component';
+import { PieceComponent } from './piece/piece.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChessBoardComponent
+    ChessBoardComponent,
+    PieceComponent
   ],
   imports: [
     BrowserModule,
---
+++ DaunoisChessReborn/src/app/piece/piece.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-piece',
+  templateUrl: './piece.component.html',
+  styleUrls: ['./piece.component.scss'],
+})
+export class PieceComponent {}
