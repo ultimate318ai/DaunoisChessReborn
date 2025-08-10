0f2fa0f171c43340a9bac39233cdc82010722b8e
Nathan DAUNOIS <nathandaunois@sfr.fr>
2025-08-10 10:11:18 +0200

    update to angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ChessService } from './boards/services/chess.service';
 import { ChessboardArrowService } from './boards/chess-board-arrow/board-arrow.service';
 import { MoveBoardComponent } from './move-board/move-board.component';
@@ -14,23 +14,16 @@ import { GameMenuComponent } from './game-menu/game-menu.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StockfishBoardComponent } from './boards/stockfish-board/stockfish-board.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    MoveBoardComponent,
-    GameStatusComponent,
-    GameMenuComponent,
-    StockfishBoardComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    DragDropModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
-  providers: [ChessService, ChessboardArrowService],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        MoveBoardComponent,
+        GameStatusComponent,
+        GameMenuComponent,
+        StockfishBoardComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        DragDropModule,
+        FormsModule,
+        ReactiveFormsModule], providers: [ChessService, ChessboardArrowService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
--- src/app/boards/services/chess.api.service.ts
+++ src/app/boards/services/chess.api.service.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpClient,
-  HttpHeaders,
-  HttpParams,
-  HttpResponse,
-} from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { boardCellNotation, PieceSymbol } from './chessTypes';
 
